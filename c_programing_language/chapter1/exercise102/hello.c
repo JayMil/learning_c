fd8e25aedae27cc4496d753a6a65bc0ec879de98
JayMil <jmilder.lp@gmail.com>
2020-04-25 02:14:44 -0500

    TCPL - Chapter 1 - Exercise 1-02: Test escape characters

---
+++ c_programing_language/chapter1/exercise102/hello.c
@@ -0,0 +1,45 @@
+/*
+ * Exercise 1-02 from
+ * The C Programming Language
+ *      by Brian W. Kernighan and Dennis M. Ritchie
+ *
+ *  By Jake Milder
+ */
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    printf("\\a: '\a'\n");
+    printf("\\b: '\b'\n");
+    printf("\\c: '\c'\n");
+    printf("\\d: '\d'\n");
+    printf("\\e: '\e'\n");
+    printf("\\f: '\f'\n");
+    printf("\\g: '\g'\n");
+    printf("\\h: '\h'\n");
+    printf("\\i: '\i'\n");
+    printf("\\j: '\j'\n");
+    printf("\\k: '\k'\n");
+    printf("\\l: '\l'\n");
+    printf("\\m: '\m'\n");
+    printf("\\n: '\n'\n");
+    printf("\\o: '\o'\n");
+    printf("\\p: '\p'\n");
+    printf("\\q: '\q'\n");
+    printf("\\r: '\r'\n");
+    printf("\\s: '\s'\n");
+    printf("\\t: '\t'\n");
+    printf("\\v: '\v'\n");
+    printf("\\w: '\w'\n");
+    printf("\\y: '\y'\n");
+    printf("\\z: '\z'\n");
+
+/*
+    // The Following examples fail with the listed errors
+    printf("\\u: '\u'\n");  //error: incomplete universal character name \u
+    printf("\\x: '\x'\n");  //error: \x used with no following hex digits
+ */
+    
+    return 0;
+}
+
