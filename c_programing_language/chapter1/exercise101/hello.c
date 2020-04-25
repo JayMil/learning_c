957c3d0b10fe60cc2efc53c279c542464ae7af95
JayMil <jmilder.lp@gmail.com>
2020-04-25 02:14:44 -0500

    TCPL - Chapter 1 - Exercise 1-01: Hello World

---
+++ c_programing_language/chapter1/exercise101/hello.c
@@ -0,0 +1,14 @@
+/*
+ * Exercise 1-01 from
+ * The C Programming Language
+ *      by Brian W. Kernighan and Dennis M. Ritchie
+ *
+ *  By Jake Milder
+ */
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    return 0;
+}
+
