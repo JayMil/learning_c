1f3b2477176e16d1c8ee94d81bcdf2bef421fc6c
JayMil <jmilder.lp@gmail.com>
2020-10-05 08:01:10 -0500

    Started bitwise tutorial

---
+++ bitwise_tutorial/bitwise.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    printf("Bitwise Operators\n");
+
+    printf("&\n");
+    printf("1 & 10: %d\n", (1 & 10));
+    printf("2 & 10: %d\n", (2 & 10));
+    printf("3 & 10: %d\n", (3 & 10));
+    printf("4 & 10: %d\n", (4 & 10));
+    printf("5 & 10: %d\n", (5 & 10));
+    printf("6 & 10: %d\n", (6 & 10));
+
+    return 0;
+}
